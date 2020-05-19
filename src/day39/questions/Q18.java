9ae81a3934f1631b191c2eb496a25c2710f4fcd7
nohurboss <muhammetm67@gmail.com>
2020-05-19 02:59:52 -0400

    JavaCourse

---
+++ src/day39/questions/Q18.java
@@ -0,0 +1,13 @@
+package day39.questions;
+
+public class Q18 {
+
+    public String  dasdas;
+    public String $;
+    public double $200_000_000_000 = 200_000_000_000.0;
+    public int dada;
+
+
+    public String technoStudyCourse;
+
+}
